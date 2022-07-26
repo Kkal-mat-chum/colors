4c7865b462fc02e4d23e560f9ab7fffd5131341c
DESKTOP-KVCQHCD\SSAFY <gksrl011@naver.com>
2022-07-26 23:09:50 +0900

    sighup연습

--- FE/colors/src/components/user/customSignUp.vue
+++ FE/colors/src/components/user/customSignUp.vue
@@ -1,9 +1,23 @@
 <template>
-  <div></div>
+  <div class="sighUpPage">
+    <div class="sighup1">드럽게 예민하네</div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style>
+.sighUpPage {
+  position: relative;
+  width: 100%;
+}
+.sighup1 {
+  position: absolute;
+  width: 50%;
+  height: 100%;
+  left: 0px;
+  top: 0px;
+}
+</style>
