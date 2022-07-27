9992213132fdf2968af65296eb47a8f61c9d7af4
min4849 <min4849@naver.com>
2022-07-27 17:40:17 +0900

    [FE] style:(65) customUpdateUser 컴포넌트-김민영

--- FE/colors/src/components/user/customUpdateUser.vue
+++ FE/colors/src/components/user/customUpdateUser.vue
@@ -5,7 +5,7 @@
       <hr class="hrStyle" />
     </div>
     <div class="update2">
-      <div class="dummyMargin"></div>
+      <div class="dummyMargin1"></div>
       <div class="modifyLabels">
         <label for="nickLabel" class="modifyLabel">닉네임</label><br />
         <label for="nameLabel" class="modifyLabel">이름</label><br />
@@ -18,9 +18,22 @@
         <input type="text" class="modifyInput" id="emailLabel" />
         <input type="text" class="modifyInput" id="pwLabel" />
       </div>
-      <div class="modifyButtons">확인버튼자리</div>
+      <div class="modifyButtons">
+        <customButton class="nickCheckBtn" id="nickCheckBtn" btnText="중복 확인" @click="testClick">testButton</customButton>
+      </div>
+      <div class="dummyMargin2"></div>
     </div>
-    <div class="update3">버튼들 자리</div>
+    <div class="update3">
+      <div class="dummyMargin3"></div>
+      <div class="modifyBottomBtns">
+        <customButton class="nickCheckBtn" id="infoChangeBtn" btnText="정보 수정" @click="testClick">testButton</customButton>
+        <div class="update3_row2">
+          <customButton class="nickCheckBtn" id="pwChangeBtn" btnText="비밀번호 변경" @click="testClick">testButton</customButton>
+          <customButton class="nickCheckBtn" id="byeBtn" btnText="회원 탈퇴" @click="testClick">testButton</customButton>
+        </div>
+      </div>
+      <div class="dummyMargin3"></div>
+    </div>
   </div>
 </template>
 
@@ -36,8 +49,8 @@ body {
   position: absolute;
   display: flex;
   flex-direction: column;
-  width: 40%;
-  height: 70%;
+  width: 35%;
+  height: 75%;
   left: 399px;
   top: 117px;
   border: 10px solid #000000;
@@ -46,7 +59,7 @@ body {
 .update1 {
   /* display: grid; */
   position: relative;
-  height: 30%;
+  height: 25%;
   /* left: 0px;
   top: 0px; */
   background: white;
@@ -56,13 +69,15 @@ body {
   display: flex;
   flex-direction: row;
   position: relative;
-  height: 55%;
+  height: 53%;
   right: 0px;
   top: 0px;
 }
 .update3 {
   position: relative;
-  height: 25%;
+  display: flex;
+  flex-direction: row;
+  height: 32%;
   right: 0px;
   top: 0px;
 }
@@ -83,32 +98,78 @@ body {
   margin: 18% 0 0 10%;
   background-color: #6667ab;
 }
-.dummyMargin {
+.dummyMargin1 {
   width: 10%;
 }
+.dummyMargin2 {
+  width: 5%;
+}
+.dummyMargin3 {
+  width: 20%;
+}
 .modifyLabels {
+  /* display: flex;
+  flex-direction: column;
+  justify-content: space-evenly; */
   width: 15%;
-  padding-left: 5%;
+  padding-left: 3%;
   text-align: left;
 }
 .modifyLabel {
   display: inline-block;
   margin: 0.6rem 0rem 2.5rem 0rem;
+  margin-top: 15%;
+  margin-bottom: 45%;
   color: #000000;
-  font-size: 18px;
+  font-size: 17px;
 }
 .modifyInputs {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   width: 50%;
 }
 .modifyInput[type="text"] {
   width: 80%;
-  margin-bottom: 2rem;
+  margin-bottom: 8%;
+  margin-top: 2%;
+  /* margin-left: 2%; */
   padding: 8px 5px;
   border-radius: 5px;
   border-color: #d0d1ff;
   font-size: 16px;
 }
 .modifyButtons {
-  width: 25%;
+  width: 20%;
+  display: flex;
+  flex-direction: row;
+}
+.nickCheckBtn {
+  padding: 0;
+}
+.update3_row2 {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.modifyBottomBtns {
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+  /* margin-bottom: 1rem; */
+}
+#nickCheckBtn {
+  height: 8%;
+}
+#infoChangeBtn {
+  margin-bottom: 5%;
+}
+#pwChangeBtn {
+  width: 35%;
+}
+#byeBtn {
+  width: 35%;
+  color: red;
 }
 </style>
