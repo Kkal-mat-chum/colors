063136aac89fcd57c40477eb4ad3a790e37e12f7
DESKTOP-KVCQHCD\SSAFY <gksrl011@naver.com>
2022-07-27 10:33:16 +0900

    [FE]SighUp page 초기 작업 (닉네임)

--- FE/colors/src/router/index.js
+++ FE/colors/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import sighUp from "../components/user/customSignUp.vue";
 
 Vue.use(VueRouter);
 
@@ -17,6 +18,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/sighup",
+    name: "sighUp",
+    component: sighUp,
   },
 ];
 
