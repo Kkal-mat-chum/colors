dd336bda3ab81abadcd7882df2bd5ded37daf2ef
sdk4590 <sdk4590@koreatech.ac.kr>
2022-07-27 23:20:08 +0900

    [FE] fix: index.js 오타 수정 - 송다경

--- FE/colors/src/router/index.js
+++ FE/colors/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import MyPageView from "../views/MyPageView.vue";
-import sighUp from "../components/user/customSignUp.vue";
+import signUp from "../components/user/customSignUp.vue";
 import logIn from "../components/user/customLogIn.vue";
 import modifyUser from "../components/user/customUpdateUser.vue";
 
