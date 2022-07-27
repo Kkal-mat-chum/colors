c609b65d723978a235f7882b63f91249fdd27c2c
chanil <kci8311@gmail.com>
2022-07-27 14:39:50 +0900

    [FE] style: (68)customButton 컴포넌트 만들기 - 김찬일

--- FE/colors/src/components/common/customButton.vue
+++ FE/colors/src/components/common/customButton.vue
@@ -1,9 +1,41 @@
 <template>
-  <div></div>
+  <button>test</button>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style>
+button {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+
+  margin: 0;
+  padding: 0.5rem 1rem;
+
+  font-family: "Pretendard";
+  text-align: center;
+  text-decoration: none;
+
+  display: inline-block;
+  width: auto;
+
+  background-color: #ffffff;
+  border-color: #d0d1ff;
+  border-radius: 4px;
+
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+
+  cursor: pointer;
+
+  transition: 0.2s;
+
+  color: #6667ab;
+}
+
+button:hover {
+  background-color: #d0d1ff;
+}
+</style>
--- FE/colors/src/views/AboutView.vue
+++ FE/colors/src/views/AboutView.vue
@@ -2,14 +2,23 @@
   <div class="about">
     <customSidebarVue></customSidebarVue>
     <h1>This is an about page</h1>
+    <customButtonVue class="testButton">testButton</customButtonVue>
   </div>
 </template>
 
 <script>
 import customSidebarVue from "@/components/common/customSidebar.vue";
+import customButtonVue from "@/components/common/customButton.vue";
 export default {
   components: {
     customSidebarVue,
+    customButtonVue,
   },
 };
 </script>
+
+<style>
+.testButton {
+  font: 2rem "pretendard Bold";
+}
+</style>
