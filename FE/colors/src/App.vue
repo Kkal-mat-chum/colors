7695ed63e98c66c16337daf8a96df993519caeaf
chanil <kci8311@gmail.com>
2022-07-27 13:15:56 +0900

    [FE] style: Pretendard 폰트 적용 - 찬일

--- FE/colors/src/App.vue
+++ FE/colors/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <router-link to="/about">Go to About</router-link>
     <router-view />
   </div>
 </template>
@@ -12,11 +13,56 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Pretendard";
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+@font-face {
+  font-family: "Pretendard";
+  src: url("@/assets/font/Pretendard-Black.otf");
+}
+
+@font-face {
+  font-family: "Pretendard";
+  src: url("@/assets/font/Pretendard-Bold.otf");
+}
+
+@font-face {
+  font-family: "Pretendard";
+  src: url("@/assets/font/Pretendard-ExtraBold.otf");
+}
+
+@font-face {
+  font-family: "Pretendard";
+  src: url("@/assets/font/Pretendard-ExtraLight.otf");
+}
+
+@font-face {
+  font-family: "Pretendard";
+  src: url("@/assets/font/Pretendard-Light.otf");
+}
+
+@font-face {
+  font-family: "Pretendard";
+  src: url("@/assets/font/Pretendard-Medium.otf");
+}
+
+@font-face {
+  font-family: "Pretendard";
+  src: url("@/assets/font/Pretendard-Regular.otf");
+}
+
+@font-face {
+  font-family: "Pretendard";
+  src: url("@/assets/font/Pretendard-SemiBold.otf");
+}
+
+@font-face {
+  font-family: "Pretendard";
+  src: url("@/assets/font/Pretendard-Thin.otf");
 }
 
 nav {
