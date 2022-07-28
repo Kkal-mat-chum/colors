0e2e962cac4023ba6ff79a2943831496b5d80dec
min4849 <min4849@naver.com>
2022-07-28 13:28:34 +0900

    [FE] Style:(59) customDeleteUser 컴포넌트 작성 - 김민영

--- FE/colors/src/components/user/customDeleteUser.vue
+++ FE/colors/src/components/user/customDeleteUser.vue
@@ -2,17 +2,24 @@
   <div>
     <div class="deleteUserPage">
       <div class="delete1">
-        <div class="title">회원 탈퇴</div>
+        <div class="titleDeleteUser">회원 탈퇴</div>
+        <hr class="deleteUserhystyle" />
       </div>
       <div class="delete2">
-        <div class="dummyMargin1"></div>
-        <div class="delteNotice">
-          <label for="deleteNotice1">※ 탈퇴를 진행할 경우, 아이디 및 데이터를 복구할 수 없습니다</label>
-          <label for="deleteNotice2">※ 개인정보는 탈퇴시 삭제됩니다.</label>
+        <div class="dummyMarginDelete1"></div>
+        <div class="deleteNotice">
+          <label for="deleteNotice1" class="deleteNotice">※ 탈퇴를 진행할 경우, 아이디 및 데이터를 복구할 수 없습니다.</label>
+          <label for="deleteNotice2" class="deleteNotice">※ 개인정보는 탈퇴시 삭제됩니다.</label>
         </div>
-        <div class="dummyMargin1"></div>
+        <div class="dummyMarginDelete1"></div>
       </div>
-      <div class="delete3">버튼들</div>
+      <div class="delete3">
+        <!-- dd -->
+        <div class="dummyMarginDelete2"></div>
+        <customButton id="deleteSureBtn" btnText="확 인" @click="testClick">testButton</customButton>
+        <customButton id="deleteCancleBtn" btnText="취 소" @click="testClick">testButton</customButton>
+        <!-- <div class="dummyMarginDelete2"></div> -->
+      </div>
     </div>
   </div>
 </template>
@@ -35,28 +42,64 @@ export default {};
   border: 10px solid #000000;
   border-radius: 10px;
 }
+.titleDeleteUser {
+  margin-top: 9%;
+  margin-left: 10%;
+  font-size: 150%;
+  font-weight: 900;
+  color: #6667ab;
+}
+.deleteUserhystyle {
+  width: 35%;
+  height: 1px;
+  background-color: #d0d1ff;
+  position: absolute;
+  margin-top: 15%;
+  margin-left: 10%;
+}
 .delete1 {
-  height: 40%;
+  height: 30%;
   display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
+  flex-direction: column;
+  /* justify-content: flex-start; */
+  align-items: flex-start;
 }
 .delete2 {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  height: 40%;
+  height: 45%;
 }
-.delteNotice {
+.deleteNotice {
   display: flex;
   flex-direction: column;
+  justify-content: space-evenly;
   width: 70%;
   text-align: left;
 }
 .delete3 {
-  height: 40%;
+  height: 45%;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
 }
-.dummyMargin1 {
+.dummyMarginDelete1 {
   width: 15%;
+}
+.dummyMarginDelete2 {
+  height: 8%;
+}
+.deleteNotice {
+  width: 100%;
+  font-size: 17px;
+}
+#deleteSureBtn {
+  color: red;
+  width: 60%;
+}
+#deleteCancleBtn {
+  width: 60%;
+  margin-top: 2%;
 }
 </style>
