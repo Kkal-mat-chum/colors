d17b870bcd99d78d70f3d0aa46ae301a825f2139
min4849 <min4849@naver.com>
2022-08-02 10:15:46 +0900

    [FE] style: (103) 원픽투표 컴포넌트 만드는 중 -김민영

--- FE/colors/src/components/Voting/colorVote.vue
+++ FE/colors/src/components/Voting/colorVote.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
+  <div class="bodyColorVote">
+    <customSidebar />
+    <div class="dummyMarginColorVote1"></div>
+    <div class="gridColorVote1">
+      <div class="timeInColorVote">11:11</div>
+      <label for="colorVoteTitle">자신과(와) 가장 어울리는 색을 선택해주세요.</label>
+      <div class="procedureInColorVote">3/5</div>
+    </div>
+    <div class="gridColorVote2">
+      <div class="imageColorVote" id="imageColorVote1"></div>
+      <div class="imageColorVote" id="imageColorVote2"></div>
+      <div class="imageColorVote" id="imageColorVote3"></div>
+      <div class="imageColorVote" id="imageColorVote4"></div>
+      <div class="imageColorVote" id="imageColorVote5"></div>
+      <div class="imageColorVote" id="imageColorVote6"></div>
+      <div class="imageColorVote" id="imageColorVote7"></div>
+      <div class="imageColorVote" id="imageColorVote8"></div>
+    </div>
+    <div class="gridColorVote3">선택완료</div>
     <i class="fa-solid fa-circle-chevron-right"></i>
   </div>
 </template>
@@ -8,4 +26,33 @@
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+.bodyColorVote {
+  border: 3px solid aqua;
+  margin-left: 120px;
+  height: 100%;
+  width: 100%;
+  /* display: flex;
+  flex-direction: column;
+  justify-content: center; */
+}
+.dummyMarginColorVote1 {
+  height: 100%;
+  width: 100%;
+  background: #000;
+}
+.gridColorVote2 {
+  display: flex;
+  flex-direction: row;
+}
+.gridColorVote1 {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+}
+.imageColorVote {
+  width: 100px;
+  height: 80px;
+  background: gray;
+}
+</style>
