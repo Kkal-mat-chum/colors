d2ffb1a17c4c958bc4bdf7a040836236ecb4aefb
min4849 <min4849@naver.com>
2022-08-02 13:02:17 +0900

    [FE] feat: (103) colorVote 원픽투표 컴포넌트 -김민영

--- FE/colors/src/components/Voting/colorVote.vue
+++ FE/colors/src/components/Voting/colorVote.vue
@@ -3,22 +3,35 @@
     <customSidebar />
     <div class="dummyMarginColorVote1"></div>
     <div class="gridColorVote1">
-      <div class="timeInColorVote">11:11</div>
-      <label for="colorVoteTitle">자신과(와) 가장 어울리는 색을 선택해주세요.</label>
-      <div class="procedureInColorVote">3/5</div>
+      <div class="labelColorVote">11:11</div>
+      <label for="colorVoteTitle" class="labelColorVote">자신과(와) 가장 어울리는 색을 선택해주세요.</label>
+      <div class="labelColorVote">3/5</div>
     </div>
     <div class="gridColorVote2">
-      <div class="imageColorVote" id="imageColorVote1"></div>
-      <div class="imageColorVote" id="imageColorVote2"></div>
-      <div class="imageColorVote" id="imageColorVote3"></div>
-      <div class="imageColorVote" id="imageColorVote4"></div>
-      <div class="imageColorVote" id="imageColorVote5"></div>
-      <div class="imageColorVote" id="imageColorVote6"></div>
-      <div class="imageColorVote" id="imageColorVote7"></div>
-      <div class="imageColorVote" id="imageColorVote8"></div>
+      <div class="gridLeftBtnColorVote1">
+        <i class="fa-solid fa-circle-chevron-right fa-flip-horizontal fa-3x leftBtnColorVote"></i>
+      </div>
+      <div class="gridPicturesColorVote">
+        <div class="gridPicturesColorVote1">
+          <div class="imageColorVote" id="imageColorVote1"></div>
+          <div class="imageColorVote" id="imageColorVote2"></div>
+          <div class="imageColorVote" id="imageColorVote3"></div>
+          <div class="imageColorVote" id="imageColorVote4"></div>
+        </div>
+        <div class="gridPicturesColorVote2">
+          <div class="imageColorVote" id="imageColorVote5"></div>
+          <div class="imageColorVote" id="imageColorVote6"></div>
+          <div class="imageColorVote" id="imageColorVote7"></div>
+          <div class="imageColorVote" id="imageColorVote8"></div>
+        </div>
+      </div>
+      <div class="gridRightBtnColorVote">
+        <i class="fa-solid fa-circle-chevron-right fa-3x rightBtnColorVote"></i>
+      </div>
     </div>
-    <div class="gridColorVote3">선택완료</div>
-    <i class="fa-solid fa-circle-chevron-right"></i>
+    <div class="gridColorVote3">
+      <customButton class="completeBtnColorVote" id="completeBtnColorVote" btnText="선택 완료" @click="testClick">선택 완료</customButton>
+    </div>
   </div>
 </template>
 
@@ -28,31 +41,89 @@ export default {};
 
 <style scoped>
 .bodyColorVote {
-  border: 3px solid aqua;
+  /* border: 3px solid aqua; */
   margin-left: 120px;
-  height: 100%;
-  width: 100%;
+  height: 730px;
+  width: 1400px;
   /* display: flex;
   flex-direction: column;
   justify-content: center; */
 }
 .dummyMarginColorVote1 {
-  height: 100%;
+  height: 10%;
   width: 100%;
-  background: #000;
+  /* display: block;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between; */
+  /* background: #000; */
 }
 .gridColorVote2 {
   display: flex;
   flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+.gridPicturesColorVote {
+  display: flex;
+  flex-direction: column;
+  padding: 50px 0;
+}
+.gridPicturesColorVote1 {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+}
+.gridPicturesColorVote2 {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
 }
 .gridColorVote1 {
   display: flex;
   flex-direction: row;
-  justify-content: space-evenly;
+  justify-content: space-around;
 }
 .imageColorVote {
-  width: 100px;
+  display: inline-block;
+  width: 280px;
+  height: 200px;
+  /* width: 200px;
+  height: 100px; */
+  /* margin-top: 150px; */
+  /* margin: 50px 10px; */
+  margin: 3px 10px;
+  border: 2px solid #d0d1ff;
+  border-radius: 15px;
+  /* margin: 5% 0% 0% 5%; */
+  filter: drop-shadow(6px 6px 4px rgba(0, 0, 0, 0.25));
+  /* width: 100px;
   height: 80px;
-  background: gray;
+  background: gray; */
+}
+.leftBtnColorVote {
+  color: #6667ab;
+  margin: 0px 5px 0px 30px;
+}
+.rightBtnColorVote {
+  color: #6667ab;
+  margin: 0px 30px 0px 5px;
+}
+.labelColorVote {
+  color: #6667ab;
+  font-size: 180%;
+  font-weight: 600;
+}
+.gridColorVote3 {
+  display: flex;
+  flex-direction: row;
+  justify-content: right;
+}
+
+#completeBtnColorVote {
+  width: 20%;
+  margin-right: 100px;
 }
 </style>
