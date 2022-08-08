9256696927cb30fd6d1a783ec122e2c2753e86b9
이한기 <gksrl011@naver.com>
2022-08-08 10:07:16 +0900

    [FE] style:113 vuex틀 작성 완료 -이한기

--- FE/colors/src/store/index.js
+++ FE/colors/src/store/index.js
@@ -1,25 +1,23 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { createApi } from "@/api";
+//import router from "@/router";
+import meetingStore from "@/store/modules/meetingStore";
+import memberStore from "@/store/modules/memberStore";
+import topicStore from "@/store/modules/topicStore";
 
 Vue.use(Vuex);
 
+export const api = createApi();
+
 export default new Vuex.Store({
-  state: {
-    imgUrlList: [
-      "assets/imagedefault/sampleimage1.png",
-      "assets/imagedefault/sampleimage2.png",
-      "assets/imagedefault/sampleimage3.png",
-      "assets/imagedefault/sampleimage4.png",
-      "assets/imagedefault/sampleimage5.png",
-      "assets/imagedefault/sampleimage6.png",
-      "assets/imagedefault/sampleimage7.png",
-      "assets/imagedefault/sampleimage8.png",
-    ],
-    selectedColorList: ["#000000", "#dddabc", "#123456", "#666123", "#555555", "#aaaaaa"],
-    teamJoinList: ["송다경", "이한기", "김찬일", "김민영", "오정환", "강민성"],
-  },
+  state: {},
   getters: {},
   mutations: {},
   actions: {},
-  modules: {},
+  modules: {
+    meetingStore,
+    memberStore,
+    topicStore,
+  },
 });
---
+++ FE/colors/src/store/modules/meetingStore.js
@@ -0,0 +1,9 @@
+//import { api } from "@/store";
+
+const meetingStore = {
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+};
+export default meetingStore;
