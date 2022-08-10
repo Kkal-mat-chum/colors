7457472c0231a871c996fa79259d4bb89fdd89ec
강민성 <pfcskms1997@naver.com>
2022-08-10 10:11:26 +0900

    [BE] feat - (132) MeetingRoom Service - 등록 API - 강민성

--- BE/colors/src/main/java/com/ssafy/colors/database/repository/RoomRepository.java
+++ BE/colors/src/main/java/com/ssafy/colors/database/repository/RoomRepository.java
@@ -1,9 +1,11 @@
 package com.ssafy.colors.database.repository;
 
 import com.ssafy.colors.database.entity.Room;
+import com.ssafy.colors.enumdata.RoomStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
+    public Room findFirstByRoomCodeAndStatus(String code, RoomStatus status);
 }
