a4f90e0198dde8c08530b61c1419b28fca2fe0a2
강민성 <pfcskms1997@naver.com>
2022-08-16 00:02:19 +0900

    [BE] fix - 오타 수정 - 강민성

--- BE/colors/src/main/java/com/ssafy/colors/controller/RoomController.java
+++ BE/colors/src/main/java/com/ssafy/colors/controller/RoomController.java
@@ -221,7 +221,7 @@ public class RoomController {
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    @POstMapping("/mypage")
+    @PostMapping("/mypage")
     public ResponseEntity<Map<String, Object>> getMyPageData(@RequestBody Map<String, Object> params) {
         System.out.println("[GET] - /room/mypage");
 
