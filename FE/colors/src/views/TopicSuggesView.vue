5c6fe94b30d1f861e112bb4338f058291f0caee9
chanil <kci8311@gmail.com>
2022-08-15 17:27:27 +0900

    [FE] fix: 토픽 오류 해결-김찬일

--- FE/colors/src/views/TopicSuggesView.vue
+++ FE/colors/src/views/TopicSuggesView.vue
@@ -34,13 +34,14 @@
             (cnt) => {
               topic.cnt = cnt;
               topic.recomend = !topic.recomend;
+              this.$router.go();
             }
           "
         />
       </topic-list>
       <div class="topTenBottomLine">
         <customButton btnText="돌아가기" @click="go" />
-        <div class="panpmgenationContainer">
+        <div class="pagenationContainer">
           <i class="material-symbols-rounded">keyboard_double_arrow_left</i>
           <i class="fa-solid fa-caret-left"></i>
           <customButton v-for="page in pages" :key="page" :btnText="page" @click="next(page)"></customButton>
