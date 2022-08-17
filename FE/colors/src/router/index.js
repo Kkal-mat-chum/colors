ab15636030e54ba541e6669d8b78c5e913c74f9d
이한기 <gksrl011@naver.com>
2022-08-17 11:25:22 +0900

    mainpage css 완성 -이한기

--- FE/colors/src/router/index.js
+++ FE/colors/src/router/index.js
@@ -10,7 +10,7 @@ import teamVoting from "../views/VotingView/TeamVoting.vue";
 import randomVoting from "../views/VotingView/RandomVoting.vue";
 import loadingwin from "@/components/Voting/loadingImg.vue";
 import MainView from "@/components/main/MainView.vue";
-import swal from "sweetalert";
+// import swal from "sweetalert";
 Vue.use(VueRouter);
 
 const routes = [
@@ -19,6 +19,11 @@ const routes = [
     name: "home",
     component: MainView,
   },
+  // {
+  //   path: "/",
+  //   name: "home",
+  //   component: HomeView,
+  // },
   {
     path: "/about",
     name: "about",
@@ -156,21 +161,21 @@ const router = new VueRouter({
   routes,
 });
 
-router.beforeEach(function (to, from, next) {
-  if (
-    to.matched.some(function (routeInfo) {
-      return routeInfo.meta.authRequired;
-    })
-  ) {
-    next();
-  } else {
-    if (sessionStorage.getItem("access-token") != null) {
-      next();
-    } else {
-      swal("로그인", "로그인이 필요한 서비스입니다.", "error");
-      next({ path: "/login" });
-    }
-  }
-});
+// router.beforeEach(function (to, from, next) {
+//   if (
+//     to.matched.some(function (routeInfo) {
+//       return routeInfo.meta.authRequired;
+//     })
+//   ) {
+//     next();
+//   } else {
+//     if (sessionStorage.getItem("access-token") != null) {
+//       next();
+//     } else {
+//       swal("로그인", "로그인이 필요한 서비스입니다.", "error");
+//       next({ path: "/" });
+//     }
+//   }
+// });
 
 export default router;
